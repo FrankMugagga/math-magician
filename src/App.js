90e99133492df90631ee7b5c0a62bb9e4502c015
FrankMugagga <frank.mugagga@gmail.com>
2023-04-07 17:36:54 +0300

    Add quote function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Calculator from './Components/Calculator';
+import DisplayQuote from './Components/displayQuote';
 
 function App() {
   return (
-    <>
+    <div id="calBody">
+      <DisplayQuote />
       <Calculator />
-    </>
+    </div>
   );
 }
 
