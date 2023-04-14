947c5e5f7916e3fff03ed4b5d22633ed3262d6ed
FrankMugagga <frank.mugagga@gmail.com>
2023-04-14 12:08:36 +0300

    modified codes

--- src/Components/_tests_/Calculator.test.js
+++ src/Components/_tests_/Calculator.test.js
@@ -3,16 +3,4 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Calculator from '../Calculator';
 
-describe('Calculator component', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<Calculator />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 
-  it('renders correctly with specific props', () => {
-    const tree = renderer
-      .create(<Calculator numberOne="5" numberTwo="3" operation="+" />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
--- src/Components/_tests_/Home.test.js
+++ src/Components/_tests_/Home.test.js
@@ -3,9 +3,4 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../Home';
 
-describe('Home component', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<Home />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
+
