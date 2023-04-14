adab4d3936f9c0db5313291504a45bd8dfeab60a
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-04-14 14:47:23 +0530

    home test updated

--- src/Components/_tests_/Home.test.js
+++ src/Components/_tests_/Home.test.js
@@ -3,4 +3,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../Home';
 
-
+describe('Home component', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
